7415a980f5eefd0b412713d9e4385be14a21971b
Google <30652896+Google987@users.noreply.github.com>
2021-03-06 11:27:57 +0530

    Add files via upload

---
+++ jsonHandling.py
@@ -0,0 +1,40 @@
+import json
+
+json_dic = {
+    "name": "a-little-coding", 
+    "url": "https://www.youtube.com/c/alittlecoding/videos"
+}
+# print(json_dic)
+# jsonString = json.dumps(json_dic) # string
+# print(jsonString)
+# print(type(jsonString))
+
+
+# json_dic = json.loads(jsonString) # python obj
+# print(json_dic)
+
+for k in json_dic.keys():
+    print(k, json_dic.get(k))
+    print(k, json_dic[k])
+
+
+
+json_with_single_quote = """{
+    'name': "a-little-coding", 
+    'url': "https://www.youtube.com/c/alittlecoding/videos"
+}"""
+print(json_with_single_quote)
+# jsonString = json.dumps(json_with_single_quote)
+# print(jsonString)
+
+# json_dic = json.loads(json_with_single_quote)
+# print(json_dic)
+
+
+import demjson
+json_dic = demjson.decode(json_with_single_quote)
+print(json_dic)
+print(type(json_dic))
+
+
+# https://stackoverflow.com/questions/4162642/single-vs-double-quotes-in-json
